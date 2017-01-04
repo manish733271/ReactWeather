3956085e4b180d4b07ded28cd81ef23859316248
Manish Adhikari <nepali.adhikari@gmail.com>
2017-01-04 12:48:26 +0100

    About updated

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,17 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className = "text-center">About</h1>
+      <p>Weather application built on react following react web app developer course</p>
+      <p>Here are the tools I used</p>
+      <ul>
+        <li>
+          <a href = "https://facebook.github.io/react" target = "_blank">React</a> - This was the Javascript framework applied.
+        </li>
+        <li>
+          <a href ="http://openweathermap.org" target = "_blank">Open Weather Map</a> - This was the Web API used
+        </li>
+      </ul>
     </div>
   )
 };
