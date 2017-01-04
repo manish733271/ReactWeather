fbb437b4dd9aed0040a8f683b3cc29b16ac473ca
Manish Adhikari <nepali.adhikari@gmail.com>
2017-01-04 19:50:12 +0100

    Sass styles added

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -18,7 +18,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3 className = "text-center">Temperature is {temp} in {location}</h3>
+    <h3 className = "text-center">Temperature is {temp} degree in {location}</h3>
   )
 };
 
